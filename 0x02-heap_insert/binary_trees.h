68f487f988af5d5fdc930460f039d4e19d1eb29f
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-03 21:14:02 -0400

    soon to be changed headers

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -23,10 +23,6 @@ typedef struct binary_tree_s heap_t;
 
 heap_t *heap_insert(heap_t **root, int value);
 
-size_t binary_tree_nodes(const binary_tree_t *tree);
-
-void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
