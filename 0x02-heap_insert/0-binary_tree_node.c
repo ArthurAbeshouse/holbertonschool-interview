d4851aa42d845c2104bf27c16cadbe8d7c22b98d
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-08-03 20:46:50 -0400

    initial push

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the node that measures the height
+ * @value: A value to which the node will be assigned.
+ * Return: The hight of the tree
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	return (new_node);
+}
