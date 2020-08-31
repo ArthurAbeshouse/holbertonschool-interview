f6e1feee74fdc766fc343d342edbdc1fad717552
ArthurAbeshouse <artieabe99@gmail.com>
2020-08-31 23:41:36 +0000

    small fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
         for line in stdin:
             if count == 10:
                 print_stats()
+                count = 0
             wrds = line.split(" ")
             size += int(wrds[8])
             status[int(wrds[7])] += 1
