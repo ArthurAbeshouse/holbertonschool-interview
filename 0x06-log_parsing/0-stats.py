2ee3eb80447cde44fc5eda937e7ded9d94f99a12
ArthurAbeshouse <artieabe99@gmail.com>
2020-09-01 01:11:14 +0000

    small tweaks

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics"""
-from sys import stdin
-
-count = 0
-size = 0
-status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-
-def print_stats():
-    print("File size: " + str(size))
-    for s in sorted(status.keys()):
-        if status[s]:
-            print(str(s) + ": " + str(status[s]))
-
-
 if __name__ == "__main__":
+    from sys import stdin
+
+    count = 0
+    size = 0
+    status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_stats():
+        print("File size: " + str(size))
+        for s in sorted(status.keys()):
+            if status[s]:
+                print(str(s) + ": " + str(status[s]))
+
     try:
         for line in stdin:
             if count == 10:
                 print_stats()
                 count = 0
             wrds = line.split()
-
             try:
                 size += int(wrds[8])
             except BaseException:
