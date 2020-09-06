978c9a8f99ee1578acd9c677f397d14c49682740
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-09-06 01:35:18 -0400

    initial push

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for a cycle in the linked list
+ * @list: pointer to the head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	listint_t *fast, *slow;
+
+	fast = slow = list;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
