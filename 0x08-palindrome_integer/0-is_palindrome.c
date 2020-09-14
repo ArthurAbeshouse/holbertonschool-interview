bfad26c0f9f13d0ceb9cb4b88cad76ddee573a15
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-09-13 23:02:32 -0400

    initial push

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if an unsigned integer is a palindrome
+ * @n: the integer that's passed through
+ * Return: 1 if n is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long n_clone, remains, reverse;
+
+	for (n_clone = n; n_clone != 0; n_clone /= 10)
+	{
+		remains = n_clone % 10;
+		reverse = reverse * 10 + remains;
+	}
+
+	return ((n == reverse) ? 1 : 0);
+}
