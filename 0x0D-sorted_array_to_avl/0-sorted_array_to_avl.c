7e687f4b82dd241c3435b80d8c353f84a667705a
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-11-02 15:48:24 -0500

    small fixes

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  * binary_tree_node - Creates a binary tree node
  *
@@ -17,7 +18,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	/*Assign data to this node*/
+	/* Assign data to this node */
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
