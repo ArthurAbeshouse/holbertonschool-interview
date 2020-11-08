e21896e2a3ac3abcb42955cc3a3a80c3f9b68f3b
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-11-08 16:44:55 -0500

    removed additonal headers

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -26,8 +26,5 @@ typedef struct skiplist_s
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
-void print_skiplist(const skiplist_t *list);
-void free_skiplist(skiplist_t *list);
-skiplist_t *create_skiplist(int *array, size_t size);
 
 #endif
