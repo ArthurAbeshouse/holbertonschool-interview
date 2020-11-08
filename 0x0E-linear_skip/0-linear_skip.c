fb92c9c85a320ff0aa11ffb22ff940c9c6faac22
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-11-08 16:43:41 -0500

    initial push

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,48 @@
+#include "search.h"
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers
+ * @list: The skip list
+ * @value: The value to search for
+ * Return: node with value, else null
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *prev = list, *now = list->express;
+	char *msg1 = "Value checked at index",
+		*msg2 = "Value found between indexes";
+
+	if (!list || !list->express)
+		return (NULL);
+
+	while (now)
+	{
+		printf("%s [%lu] = [%d]\n", msg1, now->index, now->n);
+		if (now->n >= value)
+		{
+			printf("%s [%lu] = [%zu]\n", msg2, prev->index,
+			       now->index);
+			break;
+		}
+		else if (!now->express)
+		{
+			list = now;
+			while (list->next)
+			{
+				list = list->next;
+			}
+			printf("%s [%lu] = [%zu]\n", msg2, now->index,
+			       list->index);
+		}
+		prev = now;
+		now = now->express;
+	}
+	for (; prev != NULL; prev = prev->next)
+	{
+		printf("%s [%lu] = [%d]\n", msg1, prev->index, prev->n);
+		if (value == prev->n)
+			return (prev);
+	}
+	return (NULL);
+}
