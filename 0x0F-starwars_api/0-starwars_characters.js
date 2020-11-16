99a32cbe01d2506cb0933f8e5b807e21c503e8f1
ArthurAbeshouse <artieabe99@gmail.com>
2020-11-16 05:02:56 +0000

    small tweaks

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,12 +2,16 @@
 
 const request = require('request')
 
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`, async function (error, response, body) {
-  if (error) { console.log(error) }
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, async function (error, response, body) {
+  if (error) {
+    console.log(error)
+  }
   for (const i of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
       request(i, function (error, response, body) {
-        if (error) { console.log(error) }
+        if (error) {
+	  console.log(error)
+	}
         console.log(JSON.parse(body).name)
         resolve()
       })
