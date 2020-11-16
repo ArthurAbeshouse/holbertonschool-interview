6faec6aae23a94cea325e6f1e0a6cc45acdd0a02
ArthurAbeshouse <artieabe99@gmail.com>
2020-11-16 04:27:26 +0000

    fixed synchronization

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`, function (error, response, body) {
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`, async function (error, response, body) {
   if (error) { console.log(error); }
   for (const i of JSON.parse(body).characters) {
-    request(i, function (error, response, body) {
-      if (error) { console.log(error); }
-      console.log(JSON.parse(body).name);
+    await new Promise((resolve, reject) => {
+      request(i, function (error, response, body) {
+	if (error) { console.log(error); }
+	console.log(JSON.parse(body).name);
+	resolve();
+      });
     });
   }
 });
