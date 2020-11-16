d2500751de1180a88a396910ec9b71d7d975dd5d
ArthurAbeshouse <artieabe99@gmail.com>
2020-11-16 03:58:53 +0000

    initial push

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error) { console.log(error); }
+  for (const i of JSON.parse(body).characters) {
+    request(i, function (error, response, body) {
+      if (error) { console.log(error); }
+      console.log(JSON.parse(body).name);
+      resolve();
+    });
+  }
+});
