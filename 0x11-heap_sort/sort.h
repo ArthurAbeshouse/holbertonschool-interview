666e3ee3ff86a6e5e4ca8691bdfa84cc12869a51
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-11-29 01:09:19 -0500

    reverted change

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,6 +7,6 @@
 void heap_sort(int *array, size_t size);
 void swap(int *array, size_t size, int a, int b);
 void heapify(int *array, size_t size, int index, size_t full_size);
-void print_array(int *array, size_t size);
+void print_array(const int *array, size_t size);
 
 #endif
