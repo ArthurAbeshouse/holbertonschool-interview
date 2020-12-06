55940a0db9463ab9f2da507160832e7e6fd4657e
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-12-06 15:20:21 -0500

    initial push

---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <math.h>
+
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
